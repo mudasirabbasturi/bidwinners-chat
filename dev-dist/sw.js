45362696efcc674be77ec776b8c0dbe36fa8f503
Mudasir Abbas Turi <mudasirabbas577@gmail.com>
2026-06-16 23:47:35 +0500

    update css

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-8db671c1'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.oi3norb14g8"
+    "revision": "0.pb34a0prnf"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
